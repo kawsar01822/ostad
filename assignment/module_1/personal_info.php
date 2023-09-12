3346fafd57ab242fafeaee5df04ea4af867ade5a
Kawsar Ahmed <kaws.med@gmail.com>
2023-09-12 11:50:03 +0600

    Update personal_info.php

--- assignment/module_1/personal_info.php
+++ assignment/module_1/personal_info.php
@@ -5,6 +5,6 @@
     $name = "Kawsar Ahmed";
     $age = 28;
     $country = "Bangladesh";
-    $intro = "Hello, my name is $name and I am $age years old. I am from $country. I completed my BSC in CSE from Port City International University at 2019. Currently I'm working as a Software Engineer at Datasoft Systems Bangladesh Ltd.";
+    $intro = "Hello, my name is $name and I am $age years old. I am from $country. I completed my BSC in CSE from Port City International University in 2019 at Chittagong. Currently, I'm working as a Software Engineer at Datasoft Systems Bangladesh Ltd.";
 
     echo $intro;
