941832e9df885bde13b3d7669300139b12b6d178
Kawsar Ahmed <kaws.med@gmail.com>
2023-09-26 20:30:12 +0600

    Update task_4.php

--- assignment/module_3/task_4.php
+++ assignment/module_3/task_4.php
@@ -3,16 +3,40 @@
 // Task 4: Multidimensional Array
 
 $studentGrades = [
-    "Rahim" =>  ["Math" => 85, "English" => 92, "Science" => 78],
+    "Rahim" =>  ["Math" => 45, "English" => 42, "Science" => 78],
     "Karim" =>  ["Math" => 88, "English" => 95, "Science" => 98],
-    "Kamal" =>  ["Math" => 95, "English" => 75, "Science" => 98]
+    "Kamal" =>  ["Math" => 15, "English" => 25, "Science" => 43]
 ];
 
 function calculateAverageGrades($studentsArray)
 {
     foreach ($studentsArray as $student => $grades) {
-        $average = array_sum($grades) / count($grades);
-        printf("%s's average grade is %.2f\n", $student, $average);
+        $average = round(array_sum($grades) / count($grades));
+        $grade = null;
+        
+        if($average>=80){
+            $grade = "A+";
+        }
+        else if($average>=70){
+            $grade = "A";
+        }
+        else if($average>=60){
+            $grade = "A-";
+        }
+        else if($average>=50){
+            $grade = "B";
+        }
+        else if($average>=40){
+            $grade = "C";
+        }
+        else if($average>=33){
+            $grade = "D";
+        }
+        else{
+            $grade = "F";
+        }
+
+        printf("%s's average grade is %s\n", $student, $grade);
     }
 }
 
