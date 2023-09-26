a3145ca560d838fe3cb18b16d406229f6155178e
Kawsar Ahmed <kaws.med@gmail.com>
2023-09-26 19:18:27 +0600

    Update task_3.php

--- assignment/module_3/task_3.php
+++ assignment/module_3/task_3.php
@@ -18,3 +18,13 @@ function sortDesc($number1, $number2)
 usort($grades, 'sortDesc');  // callback function
 
 print_r($grades);
+
+
+// echo PHP_EOL;
+
+// function sortGrades(array $grades){
+//     rsort($grades);         // Sorting grades in descending order
+//     print_r($grades);
+// }
+
+// sortGrades($grades);
